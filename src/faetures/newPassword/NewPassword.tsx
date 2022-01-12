ebe20828f562c3d8bf46fd4ebb0655c90edce70f
NM-va <nad.n.mwa@gmail.com>
2022-01-13 00:45:08 +0300

    change new password page

--- src/faetures/newPassword/NewPassword.tsx
+++ src/faetures/newPassword/NewPassword.tsx
@@ -1,18 +1,17 @@
-import React, { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React, {useState} from "react";
+import {useDispatch, useSelector} from "react-redux";
 import SuperInputText from "../../components/SuperInputText/SuperInputText";
-import styles from "./ForgotPassword.module.css"
+import styles from "./NewPassword.module.css"
 import SuperButton from "../../components/SuperButton/SuperButton";
 import {Link, Navigate, useParams} from "react-router-dom";
-import { createRequestRecoveryPasswordTC } from "./password-recovery-reducer";
 import {AppRootStateType} from "../../app/store";
 
-export const ForgotPassword = () => {
+export const NewPassword = () => {
     const dispatch = useDispatch();
     const [password, setPassword] = useState<string>("");
     const { token } = useParams<"token">();
     const status = useSelector<AppRootStateType, string>((state) => state.newPassword.status);
-    const error = useSelector<AppRootStateType, string>(state => state.newPassword.error);
+    // const error = useSelector<AppRootStateType, string>(state => state.newPassword.error);
     const isChangedPassword = useSelector<AppRootStateType, boolean>(state => state.newPassword.isChangedPassword);
 
     const changePassword = (value: string) => {
@@ -20,7 +19,7 @@ export const ForgotPassword = () => {
     };
 
     const createNewPassword = () => {
-        dispatch(createNewPasswordTC({password, token}));
+        // dispatch(createNewPasswordTC({password, token}));
     };
 
     if (isChangedPassword) {
@@ -39,15 +38,15 @@ export const ForgotPassword = () => {
                 <Link to={'/registration'}>Reg</Link>
                 <Link to={'/404'}>404</Link>
             </div>
-            <form>
-                <div className={"title"}>It-incubator</div>
-                <div className={"subtitle"}>Create new password</div>
-                <div className={"formControl formGroupInput"}>
-                    <SuperInputText onChangeText={changePassword} error={error} id="password"/>
-                    <label className={"labelInput"} htmlFor="password">Password</label>
+            <form className={styles.formNewPassword}>
+                <div className={styles.title}>It-incubator</div>
+                <div className={styles.subtitle}>Create new password</div>
+                <div className={styles.formEmail}>
+                    <label className={styles.labelInput} htmlFor="password">Password</label>
+                    <SuperInputText onChangeText={changePassword} id="password"/>
                 </div>
                 <div>Create new password and we will send you further instructions to email</div>
-                <div className={s.blockBtn}>
+                <div className={styles.blockBtn}>
                     <SuperButton onClick={createNewPassword} disabled={status === "loading"} type="submit" className={"primaryBtn"}>Create new password</SuperButton>
                 </div>
             </form>
