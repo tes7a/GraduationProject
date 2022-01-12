3c41e9bde10bc517b935165ad3bad8340af6521a
tes7a <ttes7a@gmail.com>
2022-01-12 09:12:44 +0200

    com app

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -10,6 +10,7 @@ import {Registration} from "../faetures/reg/Registration";
 import {TestComponent} from "../trash/TestComponent";
 
 function App() {
+    // did not change
     return (
         <Routes>
             <Route path="/" element={<Profile/>}/>
