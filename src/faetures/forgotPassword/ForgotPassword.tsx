cd6563173635c32469fb39f1ae6818f38fe4fc4d
NM-va <nad.n.mwa@gmail.com>
2022-01-14 03:22:18 +0300

    add disabled, success message

--- src/faetures/forgotPassword/ForgotPassword.tsx
+++ src/faetures/forgotPassword/ForgotPassword.tsx
@@ -1,15 +1,19 @@
 import React, {useState} from "react";
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import SuperInputText from "../../components/SuperInputText/SuperInputText";
 import styles from "./ForgotPassword.module.css"
 import SuperButton from "../../components/SuperButton/SuperButton";
 import {Link} from "react-router-dom";
 import {createForgotPasswordRequestTC} from "./forgot-password-reducer";
+import {AppRootStateType} from "../../app/store";
 
 export const ForgotPassword = () => {
     const dispatch = useDispatch();
-    const [email, setEmail] = useState("");
-    const [error, setError] = useState("");
+    const [email, setEmail] = useState<string>("");
+    const [error, setError] = useState<string>("");
+    const status = useSelector<AppRootStateType, string>((state) => state.forgotPassword.status);
+    const isCreateForgotPasswordRequest = useSelector<AppRootStateType, boolean>(state => state.newPassword.isChangedPassword);
+
     const changeEmail = (value: string) => {
         setEmail(value);
     };
@@ -35,8 +39,9 @@ export const ForgotPassword = () => {
                     <label className={styles.labelInput} htmlFor="email">Email</label>
                     <SuperInputText onChangeText={changeEmail} error={error} id="email"/>
                 </div>
+                { isCreateForgotPasswordRequest && <div style={{color: "green"}}>Success</div>}
                 <div className={styles.blockBtn}>
-                    <SuperButton onClick={sendDataForPasswordRecovery} type="submit">Send Instructions</SuperButton>
+                    <SuperButton onClick={sendDataForPasswordRecovery} disabled={status === "loading"} type="submit">Send Instructions</SuperButton>
                 </div>
                 <div>Did you remember your password?</div>
                 <div><Link to={"/login"}>Try logging in</Link></div>
