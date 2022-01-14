8dffd9da0bf3f1a71360d16aab41b99749743d96
NM-va <nad.n.mwa@gmail.com>
2022-01-14 03:23:21 +0300

    add status, error, add typization

--- src/faetures/forgotPassword/forgot-password-reducer.ts
+++ src/faetures/forgotPassword/forgot-password-reducer.ts
@@ -1,42 +1,71 @@
 import {forgotPasswordAPI} from "./forgot-password-api";
+import {Dispatch} from "redux";
 
-export const initialRecoveryPasswordState = {
+export const initialForgotPasswordState = {
     status: "idle",
+    isCreateForgotPasswordRequest: false,
+    error: ""
 };
 
-export const forgotPasswordReducer = (state:initialRecoveryPasswordStateType = initialRecoveryPasswordState, action: ActionsTypeReducer): initialRecoveryPasswordStateType => {
+export const forgotPasswordReducer = (state:initialForgotPasswordStateType = initialForgotPasswordState, action: ActionsTypeReducer): initialForgotPasswordStateType => {
     switch (action.type) {
-        case "PASSWORD-RECOVERY/SET-STATUS":
+        case "forgotPasswordReducer/SET-STATUS":
             return {...state, status: action.status};
+        case "forgotPasswordReducer/SET-IS-CREATED-PASSWORD-REQUEST":
+            return {...state, isCreateForgotPasswordRequest: action.isCreateForgotPasswordRequest};
+        case "forgotPasswordReducer/SET-ERROR":
+            return {...state, error: action.error};
         default:
             return state
     }
 };
 
 //action
-const setStatusAC = (status: RequestStatusType) => ({type: "PASSWORD-RECOVERY/SET-STATUS", status} as const);
+const setStatusAC = (status: RequestStatusType) => ({type: "forgotPasswordReducer/SET-STATUS", status} as const);
+export const setIsCreateForgotPasswordRequestAC = (isCreateForgotPasswordRequest: boolean) =>
+    ({type: "forgotPasswordReducer/SET-IS-CREATED-PASSWORD-REQUEST", isCreateForgotPasswordRequest} as const);
+export const setForgotPasswordErrorAC = (error: string) => ({type: 'forgotPasswordReducer/SET-ERROR', error} as const);
 
 //thunk
-export const createForgotPasswordRequestTC = (data: DataRecoveryPasswordType) => (dispatch: any) => {
+export const createForgotPasswordRequestTC = (data: DataForgotPasswordType) => (dispatch: ThunkDispatch) => {
     forgotPasswordAPI.createForgotPasswordRequest(data)
-        // .then(res => {
-        //
-        // })
-        // .catch(error => {
-        //
-        // })
+        .then(res => {
+            dispatch(setStatusAC('succeeded'));
+        })
+        .catch(error => {
+            if (!error.response) {
+                return "some error";
+            }
+
+            dispatch(setForgotPasswordErrorAC(error.response.data.error));
+        })
+        .finally(() => {
+            dispatch(setStatusAC('succeeded'));
+        })
 };
 
 
 //type
-type initialRecoveryPasswordStateType = typeof initialRecoveryPasswordState;
+type initialForgotPasswordStateType = typeof initialForgotPasswordState;
 export type RequestStatusType = "idle" | "loading" | "succeeded" | "failed";
-export type DataRecoveryPasswordType = {
+export type DataForgotPasswordType = {
     email: string,
     from?: string,
     message: string
 }
 
+export type ThunkDispatch = Dispatch<ActionsTypeReducer>
+
 export type setStatusType = ReturnType<typeof setStatusAC>
+export type setIsCreateForgotPasswordRequestType = ReturnType<typeof setIsCreateForgotPasswordRequestAC>
+export type setForgotPasswordErrorType = ReturnType<typeof setForgotPasswordErrorAC>
+
+export type ResponseForgotPasswordType = {
+    info: string
+    error: string
+}
+
 
 type ActionsTypeReducer = setStatusType
+    | setIsCreateForgotPasswordRequestType
+    | setForgotPasswordErrorType
