cf60bd8e49a2d62304dace6a5eadca128c807a71
NM-va <nad.n.mwa@gmail.com>
2022-01-19 20:11:21 +0300

    add file search api

--- src/faetures/search/search-api.ts
+++ src/faetures/search/search-api.ts
@@ -1,15 +1,15 @@
-import axios, {AxiosResponse} from "axios";
-import {DataNewPasswordType, ResponseNewPasswordType} from "./new-password-reducer";
+import axios from "axios";
+import {ResponseGetCardsType} from "./search-reducer";
 
 
-const instance3 = axios.create({
-    baseURL: "https://neko-back.herokuapp.com/2.0",
-    // baseURL: "http://localhost:7542/2.0/",
+const instance = axios.create({
+    // baseURL: "https://neko-back.herokuapp.com/2.0",
+    baseURL: "http://localhost:7542/2.0/",
     withCredentials: true
 });
 
-export const newPasswordAPI = {
-    createRequestRecoveryPassword(data: DataNewPasswordType){
-        return instance3.post<DataNewPasswordType, AxiosResponse<ResponseNewPasswordType>>("auth/set-new-password", data);
+export const searchAPI = {
+    searchCards(question: string){
+        return instance.get<ResponseGetCardsType>(`cards/card?${question}`);
     }
 };
