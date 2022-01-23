85d26bc08cf25d21a0f5e2cc6e9f6da92d3ef5a6
NM-va <nad.n.mwa@gmail.com>
2022-01-24 01:27:22 +0300

    min, max values range

--- src/faetures/search/SearchPack.tsx
+++ src/faetures/search/SearchPack.tsx
@@ -4,39 +4,53 @@ import React, {useState} from "react";
 import {useDebounce} from "../../hooks/useDebounce";
 import {searchCards} from "./search-reducer";
 import SuperButton from "../../components/SuperButton/SuperButton";
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {Slider} from "antd";
 import 'antd/dist/antd.css';
 import {searchPacks} from "./search-pack-reducer";
+import {getPacksTC} from "../packs/PacksReducer";
+import {AppRootStateType} from "../../app/store";
 
 
 export const SearchPack = () => {
     const dispatch = useDispatch();
     const [textSearch, setTextSearch] = useState<string>("");
-    const minValueRange = 0;
-    const maxValueRange = 40;
+    const minValueRange = useSelector<AppRootStateType, number>(state => state.packs.minCardsCount);
+    const maxValueRange = useSelector<AppRootStateType, number>(state => state.packs.maxCardsCount);
     const stepRange = 1;
     const [valueRange, setValueRange] = useState<[number, number]>([0, 40]);
 
     function valueLogging(value:any) {
         dispatch(searchPacks(value));
+        console.log("search")
     }
 
     const debouncedFunc = useDebounce(valueLogging, 2000);
 
     const searchPackBouncing = (text: string) => {
         setTextSearch(text);
-        debouncedFunc(text);
+
+        if (text.length !== 0) {
+            debouncedFunc(text);
+        }
+
+        dispatch(getPacksTC({}));
     };
 
     const searchPackSend = () => {
-        dispatch(searchPacks(textSearch));
+        if (textSearch.length !== 0) {
+            dispatch(searchPacks(textSearch));
+        }
+
+        dispatch(getPacksTC({}));
     };
 
     const onChangeRange = (newValue: [number, number]) => {
         setValueRange(newValue);
         debouncedFunc(newValue);
+        dispatch(searchPacks(newValue));
     };
+
     const wrapSlider = {
         width: "200px"
     };
