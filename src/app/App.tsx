4d950e95b19e31c274d5289bea49b6f3c4291a12
Roman-Kachura <Roman-Kachura@users.noreply.github.com>
2022-01-28 19:41:35 +0100

    Update Header

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,12 @@
 import React, {useEffect} from 'react';
+import {Header} from '../components/header/Header';
+import {NavigationApp} from './NavigationApp';
 import {useDispatch, useSelector} from "react-redux";
 import {AppRootStateType} from "./store";
+import {profileInfoTC} from "../api/AuthReducer";
 import classes from "./App.module.css";
-import {setInitialized} from "./app-reducer";
+import {Spin} from "antd";
+import {RequestStatusType, setInitialized} from "./app-reducer";
 import { PacksContainer } from '../faetures/packs/PacksContainer';
 import { CardsContainer } from '../faetures/cards/CardsContainer';
 import { ProfileContainer } from '../faetures/profile/ProfileContainer';
@@ -11,15 +15,12 @@ import { LoginContainer } from '../faetures/login/LoginContainer';
 import { NewPassword } from '../faetures/newPassword/NewPassword';
 import { ForgotPassword } from '../faetures/forgotPassword/ForgotPassword';
 import {Route, Routes } from 'react-router-dom';
-import {HeaderContainer} from "../components/header/HeaderContainer";
-
 
 
 export function App() {
     const dispatch = useDispatch();
     const isInitialized = useSelector<AppRootStateType, boolean>(state => state.app.isInitialized);
     const status: RequestStatusType = useSelector<AppRootStateType, RequestStatusType>(state => state.app.status);
-
     useEffect(() => {
         dispatch(setInitialized(true));
     }, [])
@@ -29,7 +30,7 @@ export function App() {
     }
     return (
         <div className={classes.app}>
-            <HeaderContainer/>
+            <Header/>
             <main className={classes.main}>
             <Routes>
                 <Route path={'/packs'} element={<PacksContainer/>}/>
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,14 +2,18 @@ import React, {FC} from "react";
 import {NavLink, useLocation} from "react-router-dom";
 import s from './header.module.css'
 import {RequestStatusType} from "../../app/app-reducer";
-import {linksType} from "./HeaderContainer";
 import {useSelector} from "react-redux";
 import {AppRootStateType} from "../../app/store";
+import {PATH} from "../../routes/routes";
 
-export const Header: FC<HeaderPropsType> = ({links,...props}) => {
+export const Header = () => {
     const isLoggedIn = useSelector<AppRootStateType, boolean>(state => state.auth.isLoggedIn);
     const url = useLocation().pathname;
     const status: RequestStatusType = useSelector<AppRootStateType, RequestStatusType>(state => state.app.status);
+    const links = [
+        {path:PATH.PROFILE,name:'Profile'},
+        {path:PATH.PACKS,name:'Packs'}
+    ]
 
     if (!isLoggedIn) {
         return (<></>);
@@ -20,22 +24,19 @@ export const Header: FC<HeaderPropsType> = ({links,...props}) => {
             <h3 className={s.headerTitle}>IT-INCUBATOR</h3>
             <nav className={s.nav}>
                 {
-                    links.map((l,i:number) => status !== 'loading'
+                    links.map((l, i: number) => status !== 'loading'
                         ? <NavLink
                             key={l.name + i}
                             className={`${s.headerLink} ${url === `${l.path}` ? `${s.active}` : ''}`}
                             to={l.path}
                         >
                             {l.name}
-                    </NavLink>
-                        : <div key={l.name + i} className={`${s.headerLink} ${s.headerDiv} ${url === `${l.path}` ? `${s.active}` : ''}`}>{l.name}</div>)
+                        </NavLink>
+                        : <div key={l.name + i}
+                               className={`${s.headerLink} ${s.headerDiv} ${url === `${l.path}` ? `${s.active}` : ''}`}>{l.name}</div>)
                 }
             </nav>
         </div>
     )
 
-}
-
-type HeaderPropsType = {
-    links:linksType
 }
