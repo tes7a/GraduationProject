77e6ccd93f495e007ddf60ef2e0b013d876d114c
NM-va <nad.n.mwa@gmail.com>
2022-02-10 02:02:31 +0300

    search card

--- src/faetures/search/Search.tsx
+++ src/faetures/search/Search.tsx
@@ -2,57 +2,36 @@ import styles from "../newPassword/NewPassword.module.css";
 import SuperInputText from "../../components/SuperInputText/SuperInputText";
 import React, {useState} from "react";
 import {useDebounce} from "../../hooks/useDebounce";
-import {searchCards} from "./search-reducer";
 import SuperButton from "../../components/SuperButton/SuperButton";
 import {useDispatch} from "react-redux";
-import {Slider} from "antd";
 import 'antd/dist/antd.css';
+import {setSoughtCardQuestionAC} from "./search-reducer";
 
 
 export const Search = () => {
     const dispatch = useDispatch();
     const [textSearch, setTextSearch] = useState<string>("");
-    const minValueRange = 0;
-    const maxValueRange = 40;
-    const stepRange = 1;
-    const [valueRange, setValueRange] = useState<[number, number]>([0, 40]);
 
-    function valueLogging(value:any) {
-        dispatch(searchCards(value));
+    function valueInputDebounce(value: string) {
+        dispatch(setSoughtCardQuestionAC(value));
     }
 
-    const debouncedFunc = useDebounce(valueLogging, 2000);
+    const debouncedFunc = useDebounce(valueInputDebounce, 2000);
 
-    const searchPackBouncing = (text: string) => {
+    const searchCardBouncing = (text: string) => {
         setTextSearch(text);
         debouncedFunc(text);
     };
 
-    const searchPackSend = () => {
-        dispatch(searchCards(textSearch));
+    const searchCardSend = () => {
+        dispatch(setSoughtCardQuestionAC(textSearch));
     };
-
-    const onChangeRange = (newValue: [number, number]) => {
-        setValueRange(newValue);
-        debouncedFunc(newValue);
-    };
-    const wrapSlider = {
-        width: "200px"
-    };
-
 
     return (
       <div>
           <label className={styles.labelInput} htmlFor="fieldSearch">Search</label>
-          <SuperInputText onChangeText={searchPackBouncing} id="fieldSearch"/>
-          <SuperButton onClick={searchPackSend} type="submit">Search</SuperButton>
-          <div style={wrapSlider}>
-              <Slider range min={minValueRange} max={maxValueRange}
-                      defaultValue={[0, 50]}
-                      step={stepRange}
-                      onChange={onChangeRange}
-                      value={valueRange}/>
-          </div>
+          <SuperInputText onChangeText={searchCardBouncing} id="fieldSearch"/>
+          <SuperButton onClick={searchCardSend} type="submit">Search</SuperButton>
       </div>
   )
 };
