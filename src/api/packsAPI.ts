ce77ea4efd4b24ff6903485e80b951ee4d474e7c
NM-va <nad.n.mwa@gmail.com>
2022-02-12 20:21:33 +0300

    userId change get params

--- src/api/packsAPI.ts
+++ src/api/packsAPI.ts
@@ -5,14 +5,14 @@ export const PacksAPI = {
     getPacks(data: GetDateType) {
         const page = data && data.currentPage ? data.currentPage : 1;
         const pageCount = data && data.pageCount ? data.pageCount : defaultPacksPageCount;
-        const id = data && data.id ? `user_id=${data.id}&` : '';
+        const id = data && data.id ? data.id : '';
         const sortType = data && data.sortType ? data.sortType : '';
         const min = data && data.min ? data.min : 0;
         const max = data && data.max ? data.max : 0;
         const packName = data && data.packName ? data.packName : '';
 
         return instance.get<PacksDataType, AxiosResponse<PacksDataType>, GetDateType>(
-            `/cards/pack?${id}pageCount=${pageCount}&page=${page}&sortPacks=${sortType}&min=${min}&max=${max}&packName=${packName}`
+            `/cards/pack?user_id=${id}&pageCount=${pageCount}&page=${page}&sortPacks=${sortType}&min=${min}&max=${max}&packName=${packName}`
         );
     },
     addPack(name: string) {
